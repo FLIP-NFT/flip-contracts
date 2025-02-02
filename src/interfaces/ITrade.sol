3680a51834aae3436c5e9bcf47e88b852572d600
lukema95 <lukema95@gmail.com>
2025-02-02 23:57:22 +0800

    remove unused import interfaces

--- src/interfaces/ITrade.sol
+++ src/interfaces/ITrade.sol
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "./IStorage.sol";
-import "./IPrice.sol";
 
 /**
  * @title ITrade Interface
